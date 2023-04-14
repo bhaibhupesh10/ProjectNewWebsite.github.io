7ad6feafb39c9ea01241cd2348293966ef2cb08c
Bhupesh Choudhary <72373916+bhaibhupesh10@users.noreply.github.com>
2023-04-14 23:13:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('form');
+const error = document.querySelector('#error');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+
+  if (username === 'example' && password === 'password') {
+    error.textContent = '';
+    window.location.href = 'home.html';
+  } else {
+    error.textContent = 'Invalid username or password.';
+  }
+});
